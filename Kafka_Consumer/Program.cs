76490686bb93e0ca09289ddbd1fdeba426f111f9
bel <beela93@hotmail.com>
2024-05-03 12:40:54 +0200

    Add project files.

---
+++ Kafka_Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+IConsumer<string, int> consumer = null!;
+AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
+Console.WriteLine("Hello, World!");
+Thread.Sleep(100);
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "test",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+
+using (consumer = new ConsumerBuilder<string, int>(config).Build())
+{
+    consumer.Subscribe("testTopic");
+
+    while (true)
+    {
+        var consumerResult = consumer.Consume();
+        var value = consumerResult.Message.Value;
+        Console.WriteLine($"{consumerResult.Message.Key},{value}");
+    }
+    consumer.Close();
+}
+
+
+
+void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+{
+    consumer?.Close();
+}
+//seems like if a comsumer is not closed, the Kafka broker will attempt to contact it? for a minute, before discarding it. If starting a new consumer before then, Kafka will not transmit message until to then.
---
+++ Kafka_Producer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+
+try
+{
+    // See https://aka.ms/new-console-template for more information
+
+    Console.WriteLine("Hello, World!");
+
+    var config = new ProducerConfig
+    {
+        BootstrapServers = "localhost:9092",
+    };
+    Console.WriteLine("Hello, World!");
+
+    using (var producer = new ProducerBuilder<string, int>(config).Build())
+    {
+        Console.WriteLine("Hello, World!");
+        int i = 0;
+        while(true)
+        {
+            await producer.ProduceAsync("testTopic", new Message<string, int> { Key = "testKey", Value = i }/*, Handler*/);
+            Console.WriteLine(i);
+            i++;
+            Thread.Sleep(10);
+        }
+        producer.Flush(TimeSpan.FromSeconds(10));
+    }
+    Console.WriteLine("Hello, World!");
+
+    static void Handler(DeliveryReport<string, int> deliveryReport)
+    {
+        if (deliveryReport.Error.Code is not ErrorCode.NoError)
+        {
+            Console.WriteLine(deliveryReport.Error.Reason);
+        }
+        else
+        {
+            Console.WriteLine("did it");
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
